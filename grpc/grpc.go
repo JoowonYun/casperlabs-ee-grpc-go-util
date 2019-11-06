ee1df4aec5b55afac63aed354285a7717474cdcf
Yun <iam.yunjoowon@gmail.com>
2019-11-06 11:41:21 +0900

    Support to godoc, add to comment

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,3 +1,4 @@
+// Package grpc 는 Casperlabs의 Execution Engine의 GRPC Client 모듈을 정의한 모듈이다.
 package grpc
 
 import (
@@ -13,6 +14,7 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Connect 은 Casperlabs의 Execution Engine의 unix socket으로 연결하는 함수.
 func Connect(path string) ipc.ExecutionEngineServiceClient {
 	path = `unix:////` + path
 
@@ -26,6 +28,10 @@ func Connect(path string) ipc.ExecutionEngineServiceClient {
 	return client
 }
 
+// RunGenensis 는 Execution Engine을 시작할 때 Genensis정보를 초기화하는 함수.
+//
+// genensis address, initial motes, timestamp, mintCode, posCode, validator Account 정보를 파라미터로 받아
+// RunGenensis 후 변경될 state hash와 effects를 return 받는다.
 func RunGenensis(
 	client ipc.ExecutionEngineServiceClient,
 	genesisAddress string,
@@ -70,6 +76,10 @@ func RunGenensis(
 	return genesisResult.PoststateHash, genesisResult.GetEffect().GetTransformMap()
 }
 
+// RunGenensisWithChainSpec 는 Execution Engine을 시작할 때 Genensis정보를 chain에 떄라 초기화하는 함수.
+//
+// chain name, timestamp, mintInstallCode, posInstallCode, validator Account, cost 정보를 파라미터로 받아
+// RunGenensisWithChainSpec 후 변경될 state hash와 effects를 return 받는다.
 func RunGenensisWithChainSpec(client ipc.ExecutionEngineServiceClient,
 	name string,
 	timestamp int64,
@@ -126,6 +136,10 @@ func RunGenensisWithChainSpec(client ipc.ExecutionEngineServiceClient,
 	return parentStateHash, effects, errMessage
 }
 
+// Commit 은 Execute한 effects를 적용시킬 때 사용하는 함수.
+//
+// State Hash, Execute한 effects를 파라미터로 받아,
+// Commit 후 state hash 와 현재 Bonding 된 validator의 정보를 return 받는다.
 func Commit(client ipc.ExecutionEngineServiceClient,
 	prestateHash []byte,
 	effects []*transforms.TransformEntry,
@@ -176,6 +190,9 @@ func Commit(client ipc.ExecutionEngineServiceClient,
 	return postStateHash, validators, errMessage
 }
 
+// Validate 는 WasmCode를 받아 정상 Wasm파일인지 확인해주는 함수.
+//
+// byte array의 wasm을 받아 정상 wasm 파일인지 true, false를 return 해준다.
 func Validate(client ipc.ExecutionEngineServiceClient, wasmCode []byte, protocolVersion *state.ProtocolVersion) (result bool, errMessage string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -200,6 +217,10 @@ func Validate(client ipc.ExecutionEngineServiceClient, wasmCode []byte, protocol
 	return result, errMessage
 }
 
+// Query 는 특정 state 에서 해당 Key의 path에 대한 정보를 조회해주는 함수.
+//
+// State hash, Key type, Key Data, path를 파라미터로 받아
+// Query 후 결과를 return 해준다.
 func Query(client ipc.ExecutionEngineServiceClient,
 	stateHash []byte,
 	keyType string,
@@ -243,6 +264,10 @@ func Query(client ipc.ExecutionEngineServiceClient,
 	return result, errMessage
 }
 
+// Execute 는 deploys를 실행할떄의 effects를 받아오는 함수.
+//
+// state hash, timestamp, deploys를 파라미로 받아
+// Execute 후 적용하여야할 effects를 return 해준다.
 func Execute(client ipc.ExecutionEngineServiceClient,
 	parentStateHash []byte,
 	int64timestamp int64,
@@ -277,6 +302,11 @@ func Execute(client ipc.ExecutionEngineServiceClient,
 	return effects, errMessage
 }
 
+// Upgrade 는 Wasm 코드나 Cost를 변경하여 Protocol Version을 Upgrade할 때 활용
+//
+// State hash, 변경할 Insatll Wasm코드, Cost, 현재 protocol version, 다음 protocol version을 파라미터로 받으며,
+// Install wasm 코드를 변경할지, Cost를 변경할지는 옵션으로 가능하며 Upgrade 를 통해 변경한 후
+// 변경될 state hash, effects를 return 해준다.
 func Upgrade(client ipc.ExecutionEngineServiceClient,
 	parentStateHash []byte,
 	wasmCode []byte,
@@ -327,6 +357,12 @@ func Upgrade(client ipc.ExecutionEngineServiceClient,
 	return postStateHash, effects, errMessage
 }
 
+// QueryBlanace 는 address의 balance를 조회할 때 사용하는 함수.
+//
+// 조회할 state hash와 address를 파라미터로 받아, key를 address로 Query한다.
+// name key에서 name이 mint인 uref를 추출하여 hex string로 변환하고 purse Id를 abi로 변환한 후 hex string으로 변환하여 붙인다.
+// 해당 값을 blake2b256을 하면 local bytes 값이 추출된다. 이 값을 key를 local로 하여 Query한다.
+// 받아온 uref값을 Key로 하여 Query하면 BigInt 형태의 blanace를 return 해준다.
 func QueryBlanace(client ipc.ExecutionEngineServiceClient,
 	stateHash []byte,
 	address string,
--- util/util.go
+++ util/util.go
@@ -1,3 +1,4 @@
+// Package util 은 Casperlabs의 Execution Engine과 연동시 필요한 모듈을 정의한 모듈이다.
 package util
 
 import (
@@ -13,8 +14,10 @@ import (
 	"golang.org/x/crypto/blake2b"
 )
 
+// StrEmptyStateHash 는 비어있는 trie의 state Hash 값으로 초기 state hash 값
 const StrEmptyStateHash = "3307a54ca6d5bfbafc0ef1b003f3ec4941c011ee7f79889e44416754de2f091d"
 
+// LoadWasmFile 은 wasm 파일을 byte array로 return 해주는 함수.
 func LoadWasmFile(path string) []byte {
 	wasmCode, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -24,10 +27,12 @@ func LoadWasmFile(path string) []byte {
 	return wasmCode
 }
 
+// EncodeToHexString 는 byte array를 hex string으로 변경해주는 함수.
 func EncodeToHexString(src []byte) string {
 	return hex.EncodeToString(src)
 }
 
+// DecodeHexString 는 hex string을 byte array로 변경해주는 함수.
 func DecodeHexString(str string) []byte {
 	res, err := hex.DecodeString(str)
 	if err != nil {
@@ -37,6 +42,7 @@ func DecodeHexString(str string) []byte {
 	return res
 }
 
+// Blake2b256 는 blake2b 256 hash 결과 값을 return 해주는 함수.
 func Blake2b256(ob []byte) []byte {
 	hash, err := blake2b.New256(nil)
 	if err != nil {
@@ -47,22 +53,32 @@ func Blake2b256(ob []byte) []byte {
 	return hash.Sum(nil)
 }
 
+// MakeProtocolVersion 은 major, minor, patch의 값을 받아 ProtocolVersion 을 만들어주는 함수
 func MakeProtocolVersion(major uint32, minor uint32, patch uint32) *state.ProtocolVersion {
 	return &state.ProtocolVersion{Major: uint32(major), Minor: uint32(minor), Patch: uint32(patch)}
 }
 
+// AbiUint32ToBytes 는 uint32 형식을 Abi 형태인 byte array로 변경해주는 함수.
+//
+// little endian의 uint32형태로 넣는다.
 func AbiUint32ToBytes(src uint32) []byte {
 	res := make([]byte, 4)
 	binary.LittleEndian.PutUint32(res, src)
 	return res
 }
 
+// AbiUint64ToBytes 는 uint64 형식을 Abi 형태인 byte array로 변경해주는 함수.
+//
+// little endian의 uint64형태로 넣는다.
 func AbiUint64ToBytes(src uint64) []byte {
 	res := make([]byte, 8)
 	binary.LittleEndian.PutUint64(res, src)
 	return res
 }
 
+// AbiBytesToBytes 는 byte array 형식을 Abi 형태인 byte array로 변경해주는 함수.
+//
+// byte array의 길이를 little endian의 uint32형태로 넣고, 그 뒤 src 내용을 붙인다.
 func AbiBytesToBytes(src []byte) []byte {
 	res := make([]byte, 4)
 	binary.LittleEndian.PutUint32(res, uint32(len(src)))
@@ -70,6 +86,9 @@ func AbiBytesToBytes(src []byte) []byte {
 	return res
 }
 
+// AbiOptionToBytes 는 byte array 형식에 Abi 형태인 Option을 추가한 byte array로 변경해주는 함수.
+//
+// byte array에서 값이 있으면 앞에 1을 추가하고 없으면 0을 추가한다.
 func AbiOptionToBytes(src []byte) []byte {
 	res := make([]byte, 1)
 	if len(src) > 0 {
@@ -80,6 +99,9 @@ func AbiOptionToBytes(src []byte) []byte {
 	return res
 }
 
+// AbiStringToBytes 는 string 형식에 Abi 형태인 byte array로 변경해주는 함수.
+//
+// string의 length를 little endian의 uint32형태로 넣고, src를 utf8인코딩의 byte array로 변환하여 붙인다.
 func AbiStringToBytes(src string) []byte {
 	res := make([]byte, 4)
 	binary.LittleEndian.PutUint32(res, uint32(len(src)))
@@ -87,6 +109,10 @@ func AbiStringToBytes(src string) []byte {
 	return res
 }
 
+// AbiMakeArgs 는 deploy에 사용할 Args를 abi 형태인 byte array로 변경해주는 함수.
+//
+// Args 개 수를 little endian의 uint32형태로 넣고, 각 Arg를 순차적으로 붙인다.
+// 이 때 Arg의 length를 little endian의 uint32형태로 넣고, data 내용을 붙인다.
 func AbiMakeArgs(src [][]byte) []byte {
 	res := make([]byte, 4)
 	binary.LittleEndian.PutUint32(res, uint32(len(src)))
@@ -101,6 +127,9 @@ func AbiMakeArgs(src [][]byte) []byte {
 	return res
 }
 
+// AbiBigIntTobytes 는 big.Int 형식을 Abi 형태인 byte array로 변경해주는 함수.
+//
+// big.Int를 byte array로 변환 후 reverse하고, 해당 length를 맨 앞에 추가해준다.
 func AbiBigIntTobytes(src *big.Int) []byte {
 	bytes := reverseBytes(src.Bytes())
 	res := []byte{byte(len(bytes))}
@@ -120,6 +149,10 @@ func reverseBytes(src []byte) []byte {
 	return src
 }
 
+// MakeArgsTransferToAccount 는 transfer_to_account.wasm을 사용할 때 Args를 만드는 함수.
+//
+// string의 수신자 address를 받아 byte array로 변환하고 abi 형태로 만든다, amount 또한 abi 형태로 만든다.
+// 이 후 2개의 abi를 AbiMakeArgs를 통해 하나의 Abi args로 만들어 return 해준다.
 func MakeArgsTransferToAccount(address string, amount uint64) []byte {
 	addressBytes := DecodeHexString(address)
 	addressAbi := AbiBytesToBytes(addressBytes)
@@ -128,24 +161,38 @@ func MakeArgsTransferToAccount(address string, amount uint64) []byte {
 	return AbiMakeArgs(sessionAbiList)
 }
 
+// MakeArgsStandardPayment 는 standard_payment.wasm을 사용할 때 Args를 만드는 함수.
+//
+// big.Int은 amount를 받아 Abi형태로 만들고, AbiMakeArgs를 통해 Abi args로 만들어 return 해준다.
 func MakeArgsStandardPayment(amount *big.Int) []byte {
 	paymentAbiList := [][]byte{AbiBigIntTobytes(amount)}
 	paymentAbi := AbiMakeArgs(paymentAbiList)
 	return paymentAbi
 }
 
+// MakeArgsBonding 은 bonding.wasm을 사용할 때 Args를 만드는 함수.
+//
+// uint64의 amount를 받아 Abi형태로 만들고, AbiMakeArgs를 통해 Abi args로 만들어 return 해준다.
 func MakeArgsBonding(amount uint64) []byte {
 	abiList := [][]byte{AbiUint64ToBytes(amount)}
 	abi := AbiMakeArgs(abiList)
 	return abi
 }
 
+// MakeArgsUnBonding 은 unbonding.wasm을 사용할 때 Args를 만드는 함수.
+//
+// uint64의 amount를 받아 Abi형태로 만들고, Option Abi를 추가한다. 이 후 AbiMakeArgs를 통해 Abi args로 만들어 return 해준다.
 func MakeArgsUnBonding(amount uint64) []byte {
 	abiList := [][]byte{AbiOptionToBytes(AbiUint64ToBytes(amount))}
 	abi := AbiMakeArgs(abiList)
 	return abi
 }
 
+// MakeDeploy 는 address, sessionCode, sessionArgs, paymentCode, paymentArgs, gasPrice, timestamp를 받아 DeployItem을 만들어주는 함수.
+//
+// Seesion, Payment 데이터로 Deploy Body를 만들고 이를 Marshal한 값을 Blake2b256 Hash를 하여 Deploy Body Hash 값을 만든다.
+// Deploy Body Hash 값을 포함한 Deploy Header 값을 만들고 이를 Marshal한 값을 Blake2b256 Hash하여 Deploy Header Hash 값을 만든다.
+// Deploy Header Hash 값을 Deploy Item을 만들고 return 해준다.
 func MakeDeploy(
 	strFromAddress string,
 	sessionCode []byte,
@@ -184,10 +231,12 @@ func MakeDeploy(
 	return deploy
 }
 
+// MakeInitDeploys 은 Deploy Item array를 할당 받기위한 함수.
 func MakeInitDeploys() []*ipc.DeployItem {
 	return []*ipc.DeployItem{}
 }
 
+// AddDeploy 는 deploy를 deploy list에 추가하기위한 함수.
 func AddDeploy(deploys []*ipc.DeployItem, deploy *ipc.DeployItem) []*ipc.DeployItem {
 	return append(deploys, deploy)
 }
