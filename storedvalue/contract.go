97dd433b20bdbf0203fcc18c75504ba0b469c8cb
Joowon <iam.yunjoowon@gmail.com>
2020-03-26 16:03:54 +0900

    feat: support to contract stored value

--- storedvalue/contract.go
+++ storedvalue/contract.go
@@ -1,5 +1,19 @@
 package storedvalue
 
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/hdac-io/casperlabs-ee-grpc-go-util/protobuf/io/casperlabs/casper/consensus/state"
+)
+
+const (
+	PROTOCOL_VERSION_MAJOR_LENGTH = 4
+	PROTOCOL_VERSION_MINOR_LENGTH = 4
+	PROTOCOL_VERSION_PATCH_LENGTH = 4
+	PROTOCOL_VERSION_LENGTH       = PROTOCOL_VERSION_MAJOR_LENGTH + PROTOCOL_VERSION_MINOR_LENGTH + PROTOCOL_VERSION_PATCH_LENGTH
+)
+
 type Contract struct {
 	Body            []byte          `json:"body"`
 	NamedKeys       []NamedKey      `json:"named_key"`
@@ -14,9 +28,86 @@ func NewContract(body []byte, namedKeys []NamedKey, protocolVersion ProtocolVers
 	}
 }
 
-// TODO Need to check..
 func (c Contract) FromBytes(src []byte) (contract Contract, err error, pos int) {
-	return Contract{}, nil, pos
+	pos = 0
+	bodySizeBytes := src[pos : pos+SIZE_LENGTH]
+	pos += SIZE_LENGTH
+	bodySize := int(binary.LittleEndian.Uint32(bodySizeBytes))
+	body := src[pos : pos+bodySize]
+	pos += bodySize
+
+	// NamedKeys
+	namedKeys := []NamedKey{}
+	namedKeysSizeBytes := src[pos : pos+SIZE_LENGTH]
+	pos += SIZE_LENGTH
+	namedKeysSize := int(binary.LittleEndian.Uint32(namedKeysSizeBytes))
+	for i := 0; i < namedKeysSize; i++ {
+		var namedKey NamedKey
+		namedKey, err, length := namedKey.FromBytes(src[pos:])
+		if err != nil {
+			return Contract{}, err, pos
+		}
+		pos += length
+
+		namedKeys = append(namedKeys, namedKey)
+	}
+
+	var protocolVersion ProtocolVersion
+	protocolVersion, err, length := protocolVersion.FromBytes(src[pos:])
+	pos += length
+	if err != nil {
+		return Contract{}, err, pos
+	}
+
+	c = NewContract(body, namedKeys, protocolVersion)
+	return c, nil, pos
+}
+
+func (c Contract) ToBytes() []byte {
+	res := make([]byte, SIZE_LENGTH)
+	binary.LittleEndian.PutUint32(res, uint32(len(c.Body)))
+
+	namedKeysLengthBytes := make([]byte, SIZE_LENGTH)
+	binary.LittleEndian.PutUint32(namedKeysLengthBytes, uint32(len(c.NamedKeys)))
+	res = append(res, namedKeysLengthBytes...)
+	for _, namedKey := range c.NamedKeys {
+		res = append(res, namedKey.ToBytes()...)
+	}
+
+	res = append(res, c.ProtocolVersion.ToBytes()...)
+
+	return res
+}
+
+func (c Contract) ToStateValue() *state.Contract {
+	stateNamedKeys := []*state.NamedKey{}
+	for _, namedKey := range c.NamedKeys {
+		stateNamedKeys = append(stateNamedKeys, namedKey.ToStateValue())
+	}
+
+	return &state.Contract{
+		Body:            c.Body,
+		NamedKeys:       stateNamedKeys,
+		ProtocolVersion: c.ProtocolVersion.ToStateValue(),
+	}
+}
+
+func (c Contract) FromStateValue(state *state.Contract) (Contract, error) {
+	namedKeys := []NamedKey{}
+	for _, stateNamedKey := range state.GetNamedKeys() {
+		var namedKey NamedKey
+		namedKey, err := namedKey.FromStateValue(stateNamedKey)
+		if err != nil {
+			return Contract{}, nil
+		}
+		namedKeys = append(namedKeys, namedKey)
+	}
+
+	return NewContract(
+		state.Body,
+		namedKeys,
+		NewProtocolVersion(state.ProtocolVersion.GetMajor(), state.ProtocolVersion.GetMinor(), state.ProtocolVersion.GetPatch()),
+	), nil
 }
 
 type ProtocolVersion struct {
@@ -30,5 +121,42 @@ func NewProtocolVersion(major uint32, minor uint32, patch uint32) ProtocolVersio
 		Major: major,
 		Minor: minor,
 		Patch: patch,
+	}
+}
+
+func (p ProtocolVersion) FromBytes(src []byte) (protocolVersion ProtocolVersion, err error, pos int) {
+	pos = 0
+	if len(src) != PROTOCOL_VERSION_LENGTH {
+		return ProtocolVersion{}, fmt.Errorf("ActionThresholds must be %d, but %d", PROTOCOL_VERSION_LENGTH, len(src)), pos
+	}
+
+	major := binary.LittleEndian.Uint32(src[pos : pos+PROTOCOL_VERSION_MAJOR_LENGTH])
+	pos += PROTOCOL_VERSION_MAJOR_LENGTH
+	minor := binary.LittleEndian.Uint32(src[pos : pos+PROTOCOL_VERSION_MINOR_LENGTH])
+	pos += PROTOCOL_VERSION_MINOR_LENGTH
+	patch := binary.LittleEndian.Uint32(src[pos : pos+PROTOCOL_VERSION_PATCH_LENGTH])
+	pos += PROTOCOL_VERSION_PATCH_LENGTH
+
+	p = NewProtocolVersion(major, minor, patch)
+
+	return p, nil, pos
+}
+
+func (p ProtocolVersion) ToBytes() []byte {
+	pos := 0
+	res := make([]byte, PROTOCOL_VERSION_LENGTH)
+	binary.LittleEndian.PutUint32(res[pos:pos+PROTOCOL_VERSION_MAJOR_LENGTH], p.Major)
+	pos += PROTOCOL_VERSION_MAJOR_LENGTH
+	binary.LittleEndian.PutUint32(res[pos:pos+PROTOCOL_VERSION_MINOR_LENGTH], p.Minor)
+	pos += PROTOCOL_VERSION_PATCH_LENGTH
+	binary.LittleEndian.PutUint32(res[pos:pos+PROTOCOL_VERSION_PATCH_LENGTH], p.Patch)
+	return res
+}
+
+func (p ProtocolVersion) ToStateValue() *state.ProtocolVersion {
+	return &state.ProtocolVersion{
+		Major: p.Major,
+		Minor: p.Minor,
+		Patch: p.Patch,
 	}
 }
