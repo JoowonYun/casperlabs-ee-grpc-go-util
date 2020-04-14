e952725ab49bfff0dc875fb0ccec01b3a3efb91a
Joowon <iam.yunjoowon@gmail.com>
2020-04-14 12:01:53 +0900

    feat: support to claim

--- storedvalue/key_test.go
+++ storedvalue/key_test.go
@@ -209,3 +209,46 @@ func TestNamedKeysGetDelegateFromDelegators(t *testing.T) {
 	assert.Equal(t, "1000000000000000000", delegators["93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915"])
 	assert.Equal(t, "2000000000000000000", delegators["51f1ddda0933696150cf78fe7a2141653e6a841d2f4ecaaa915a299cb7a4d19c"])
 }
+
+func TestNamedKeysGetCommission(t *testing.T) {
+	namedkeys := NamedKeys{
+		NamedKey{Name: "d_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_1000000000000000000", Key: Key{}},
+		NamedKey{Name: "v_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_4000000000000000000", Key: Key{}},
+		NamedKey{Name: "d_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_1000000000000000000", Key: Key{}},
+		NamedKey{Name: "c_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_100000000000000000", Key: Key{}},
+		NamedKey{Name: "v_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_3000000000000000000", Key: Key{}},
+		NamedKey{Name: "pos_bonding_purse", Key: Key{}},
+		NamedKey{Name: "d_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_51f1ddda0933696150cf78fe7a2141653e6a841d2f4ecaaa915a299cb7a4d19c_2000000000000000000", Key: Key{}},
+		NamedKey{Name: "d_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_51f1ddda0933696150cf78fe7a2141653e6a841d2f4ecaaa915a299cb7a4d19c_3000000000000000000", Key: Key{}},
+	}
+	address, err := hex.DecodeString("93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915")
+	assert.NoError(t, err)
+
+	values := namedkeys.GetValidatorCommission(address)
+
+	assert.Equal(t, "100000000000000000", values)
+}
+
+func TestNamedKeysGetReward(t *testing.T) {
+	namedkeys := NamedKeys{
+		NamedKey{Name: "d_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_1000000000000000000", Key: Key{}},
+		NamedKey{Name: "v_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_4000000000000000000", Key: Key{}},
+		NamedKey{Name: "d_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_1000000000000000000", Key: Key{}},
+		NamedKey{Name: "c_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_100000000000000000", Key: Key{}},
+		NamedKey{Name: "r_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_200000000000000000", Key: Key{}},
+		NamedKey{Name: "r_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_300000000000000000", Key: Key{}},
+		NamedKey{Name: "v_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_3000000000000000000", Key: Key{}},
+		NamedKey{Name: "pos_bonding_purse", Key: Key{}},
+		NamedKey{Name: "d_93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915_51f1ddda0933696150cf78fe7a2141653e6a841d2f4ecaaa915a299cb7a4d19c_2000000000000000000", Key: Key{}},
+		NamedKey{Name: "d_d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84_51f1ddda0933696150cf78fe7a2141653e6a841d2f4ecaaa915a299cb7a4d19c_3000000000000000000", Key: Key{}},
+	}
+	address1, err := hex.DecodeString("93236a9263d2ac6198c5ed211774c745d5dc62a910cb84276f8a7c4959208915")
+	address2, err := hex.DecodeString("d70243dd9d0d646fd6df282a8f7a8fa05a6629bec01d8024c3611eb1c1fb9f84")
+	assert.NoError(t, err)
+
+	address1Values := namedkeys.GetUserReward(address1)
+	address2Values := namedkeys.GetUserReward(address2)
+
+	assert.Equal(t, "200000000000000000", address1Values)
+	assert.Equal(t, "300000000000000000", address2Values)
+}
